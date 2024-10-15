a44ebbfba78f69ddc66ecf7c7c03f5e5cd4c38f8
Sangeethajaichandran <sangeetha_er84@yahoo.co.in>
2024-10-15 21:44:11 +0530

    Add files via upload
    Day1-Assignment

---
+++ Chrome.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class Chrome 
+{
+	public static void main(String[] args) 
+	{
+		//Primitive Data Type
+	float version=91.0f;
+	String developer="Google";
+	boolean isBeta=false;//camelCase
+	int releaseYear=2008;//camelCase
+	char shortcutKey='C';//camelCase
+	
+	System.out.println(version);
+	System.out.println("developer:"+ developer);
+	System.out.println(isBeta);
+	System.out.println("Release Year:"+releaseYear);
+	System.out.println(shortcutKey);
+	
+	}
+
+}
